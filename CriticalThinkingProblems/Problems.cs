2180d9c85a353615fdcbc7f29f4914785dabb606
Jackson Smith <jacksonsmith7343@gmail.com>
2020-04-24 14:17:49 -0500

    Printed a string with only the characters divisible by 3.

--- CriticalThinkingProblems/Problems.cs
+++ CriticalThinkingProblems/Problems.cs
@@ -19,24 +19,31 @@ namespace CriticalThinkingProblems
         public void Run()
         {
             ForLoop("Hello World");
+            //WhileLoop();
         }
 
         public void ForLoop(string word)
         {
             
            
-            for (int i = 0; i < word.Length; i++)
+            for (int i = 0; i< word.Length; i ++)
             {
-                Console.WriteLine(i);
-                Console.ReadLine();
+                if(i %3 == 0)
+                Console.WriteLine(word[i]);
             }
-            
+            Console.ReadLine();
+
         }
         
 
         public void WhileLoop()
         {
-
+            int counter = 5;
+            while (counter < 25)
+            {
+                counter++;
+            }
+            Console.ReadLine();
         }
 
     }
