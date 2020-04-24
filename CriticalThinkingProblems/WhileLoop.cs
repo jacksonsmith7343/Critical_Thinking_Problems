05b9b79a39ce6e74837a889ed416e9caee57df6e
Jackson Smith <jacksonsmith7343@gmail.com>
2020-04-24 10:01:18 -0500

    Initialize critical thinking problems.

---
+++ CriticalThinkingProblems/WhileLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingProblems
+{
+    class WhileLoop
+    {
+        //member variables 
+
+        //constructor
+
+        //member variables 
+        public void WhileLoopPractice()
+        {
+
+        }
+    }
+}
